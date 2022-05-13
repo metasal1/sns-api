4dbdc3512671800983204c5c3fe4c72a44a6797a
Salim Karim <imsjk8@gmail.com>
2022-05-13 13:59:44 +1000

    more CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    colors: {
+      primary: '#9945FF',
+      secondary: '#14F195',
+      white: '#FFFFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
